6bc43c7d012d5b09bc2ae284380437bf11e8a5b4
Lalinkce <lalinkce@gmail.com>
2016-01-06 22:46:23 +0100

    Primera subida

--- app/src/main/java/iuAndroid/MainActivity.java
+++ app/src/main/java/iuAndroid/MainActivity.java
@@ -1,25 +1,46 @@
 package iuAndroid;
 
 import timetracker.iuandroid.R;
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.support.design.widget.TabLayout;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
-	private Toolbar mToolbar;
+	private Toolbar toolbar;
+	private TabLayout tabLayout;
+	private ViewPager viewPager;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//setSupportActionBar(mToolbar);
+		toolbar = (Toolbar) findViewById(R.id.action_bar_activity_main);
+		setSupportActionBar(toolbar);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+		viewPager = (ViewPager) findViewById(R.id.viewpager);
+		setupViewPager(viewPager);
+
+		tabLayout = (TabLayout) findViewById(R.id.tabs);
+		tabLayout.setupWithViewPager(viewPager);
+
 		Button butoPerComensar = (Button) findViewById(R.id.button1);
 		butoPerComensar.setOnClickListener(butoPerComensarListener);
 	}
@@ -30,12 +51,47 @@ public class MainActivity extends AppCompatActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
+
 	private OnClickListener butoPerComensarListener = new OnClickListener() {
 		public void onClick(View v) {
 			Intent intent = new Intent(MainActivity.this, LlistaActivitatsActivity.class);
 			startActivity(intent);
 		}
 	};
+	private void setupViewPager(ViewPager viewPager) {
+		ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+		adapter.addFragment(new OneFragment(), "Projectes");
+		adapter.addFragment(new TwoFragment(), "Tasques");
+		viewPager.setAdapter(adapter);
+	}
+
+	class ViewPagerAdapter extends FragmentPagerAdapter {
+		private final List<Fragment> mFragmentList = new ArrayList<>();
+		private final List<String> mFragmentTitleList = new ArrayList<>();
+
+		public ViewPagerAdapter(FragmentManager manager) {
+			super(manager);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			return mFragmentList.get(position);
+		}
+
+		@Override
+		public int getCount() {
+			return mFragmentList.size();
+		}
+
+		public void addFragment(Fragment fragment, String title) {
+			mFragmentList.add(fragment);
+			mFragmentTitleList.add(title);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return mFragmentTitleList.get(position);
+		}
+	}
 
 }
